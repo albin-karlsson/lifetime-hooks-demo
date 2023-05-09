db821b331004b4d82ea80f7b6788c8710e83049b
Albin Karlsson <55614148+albin-karlsson@users.noreply.github.com>
2023-05-09 13:53:00 +0200

    Update app again

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import Pokemon from "./components/Pokemon";
 
 function App() {
   const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
 
+  // This useEffect will run when the page loads
   useEffect(() => {
     fetch("http://localhost:7000/pokemons")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
 
+  // This useEffect will run when the variable count changes
+  useEffect(() => {
+    console.log("Count changed!");
+  }, [count]);
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>Count!</button>
+
       <div className="grid-container">
         {data.map((p) => (
           <Pokemon
